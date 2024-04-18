a7f58da3990f1d59ff588d23ea615fb89b7503d7
Maria Shalunova <shalunova00@gmail.com>
2024-04-18 23:09:37 +0300

    update step 9

---
+++ src/games/gamePrime.js
@@ -0,0 +1,19 @@
+import { getRandomInt, isPrime } from '../helpers.js';
+import runGame from '../index.js';
+
+const description = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+const minNumber = 1;
+const maxNumber = 99;
+
+const getRound = () => {
+  const question = getRandomInt(minNumber, maxNumber);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
+
+const runGamePrime = () => {
+  runGame(description, getRound);
+};
+
+export default runGamePrime;
