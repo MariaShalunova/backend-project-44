ca9924e921e031d3ec9658b9935b5351b464b36e
Maria Shalunova <shalunova00@gmail.com>
2024-04-23 21:19:17 +0300

    make fix

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,1 @@
- /* eslint-disable import/prefer-default-export */
-
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
--- src/welcome.js
+++ src/welcome.js
@@ -1,5 +1,3 @@
- /* eslint-disable import/prefer-default-export */
-
 import readlineSync from 'readline-sync';
 
 export const runWelcome = () => {
