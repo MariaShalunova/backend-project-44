91de6451aa8edbce6de7e81d535c496429cfd0f0
Maria Shalunova <shalunova00@gmail.com>
2024-04-23 20:48:33 +0300

    change min max

--- src/games/even.js
+++ src/games/even.js
@@ -2,13 +2,11 @@ import { getRandomNumber } from '../helpers.js';
 import { runGame } from '../index.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-const minNumber = 1;
-const maxNumber = 999;
 
 const isEven = (randomNumber) => randomNumber % 2 === 0;
 
 const getRound = () => {
-  const question = getRandomNumber(minNumber, maxNumber);
+  const question = getRandomNumber(1, 999);
   const answer = isEven(question) ? 'yes' : 'no';
 
   return [question, answer];
